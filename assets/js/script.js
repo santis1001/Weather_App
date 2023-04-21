f4d0e8418ab584e6533783ae2376b69aa386e3a4
SV <72838881+santis1001@users.noreply.github.com>
2023-04-21 16:40:52 -0600

    JS Comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 
+//API key
 var appid = 'f5cf5cc638b6ea3d999d0a5599e54ea5'
-var maybe = 'b1b15e88fa797225412429c1c50c122a1r';
-var isMain = false;
+
+//API call mock data
 var mockAPI5 = './assets/js/mock_data5.json'
 var mockAPI1 = './assets/js/mock_data1.json'
-
-var lat = 41.8338;
-var lon = -88.0616;
+//API call address
 var webAPI5 = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+appid+'&units=metric'
 var webAPI1 = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+appid+'&units=metric'
 
-//console.log(webAPI1);
-
-var k = -273.15;
+//Initialize HTML elements variables
 var search_btn = document.getElementById('search_btn');
 var search_bar = document.getElementById('search_bar');
 $('#search_btn').on('click', function(event){search_param(event, 'btn') });
@@ -29,7 +26,7 @@ var doc_today_temp = document.getElementById('temp');
 var doc_today_wind = document.getElementById('wind');
 var doc_today_humd = document.getElementById('humd');
 
-// localStorage.clear();
+//Initialize Local Global variables
 var Selected_city_name ;
 var autocomlete_city = [];
 var historyCity = [];
@@ -39,7 +36,9 @@ var Selected_city_today = {};
 var Selected_city_5day = [];
 
 var btn_array;
-
+//fill arrays with their information
+//btn_array contains the object with necessary information to use the webAPI
+//historyCiti contains the name of previously searched cities
 btn_array = JSON.parse(localStorage.getItem('OBJHistory'));
 historyCity =JSON.parse(localStorage.getItem('CityHistory'));
 if(historyCity==null){
@@ -60,7 +59,8 @@ if(btn_array==null){
 
     render_btn();
 }
-
+//This function fetch and stores the world cities into an large array
+//which will be used for the autocomplete function for the search input
 $(
     function(){
         fetch('./assets/js/worldcities.csv')
@@ -87,7 +87,9 @@ $(
         .catch(error => console.log(error));
     }
 );
-
+//this functino is calles from the `search_weather_info` function and 
+//will retrieve data for the main dashboard information using the `todayCast` function
+//which will render the information in the HTML
 function call_API1(web) {
     data = fetch(web)
     .then(response => response.json())
@@ -96,6 +98,9 @@ function call_API1(web) {
         todayCast(data);
     });
 } 
+//this functino is calles from the `search_weather_info` function and 
+//will retrieve data for the 5 day forecast information cards using `Five_dayCast` function
+//which will render the information in the HTML
 function call_API5(web){
     data = fetch(web)
     .then(response => response.json())
@@ -105,6 +110,8 @@ function call_API5(web){
         Five_dayCast(data.list);
     });
 }
+//this function is triggered from the search input and button
+//it will recieve the event and a string pointing which made the click
 function search_param (src_data, src){
     //console.log(src_data.type +' '+src);
     var value_search = document.getElementById('search_bar');
@@ -120,40 +127,48 @@ function search_param (src_data, src){
         search_btn.setAttribute('class', 'mt-2 w-100 btn btn-primary');
     }
 }
+//if the event trigger was the button this function will be called and sent 
+//data from the input value
+//this functions is called from the history buttons aswell
+//this function compares the data recieved and the autocomplete so there isnt 
+//a input that doesnt exist.
+//if exist it will save the data in the btn_array and historyCity and save it 
+//in the LocalStorage then render the history button which will include the new
+//one the render the information for the dashboard
 function search_fetch(input_data){
     
     const index =  autocomlete_city.indexOf(input_data);
-    
-    Selected_city_info = {
-            name:city_list[index].city_ascii,
-            lat:city_list[index].lat, 
-            lng: city_list[index].lng,
-            country: city_list[index].iso2
-        };        
-    console.log(historyCity.indexOf(input_data));
+    try{
+        Selected_city_info = {
+                name:city_list[index].city_ascii,
+                lat:city_list[index].lat, 
+                lng: city_list[index].lng,
+                country: city_list[index].iso2
+            };        
+    //console.log(historyCity.indexOf(input_data));
     if(historyCity.indexOf(input_data)==(-1)){
         historyCity.push(input_data);
         console.log(historyCity);
         localStorage.setItem('CityHistory', JSON.stringify(historyCity));
-
-        console.log(Selected_city_info);   
+        //console.log(Selected_city_info);   
             
         btn_array = JSON.parse(localStorage.getItem('OBJHistory'));
         if(btn_array==null){        
             btn_array=[];
         }
-
         btn_array.push(Selected_city_info);
         console.log(btn_array);
         localStorage.setItem('OBJHistory', JSON.stringify(btn_array));
         
         render_btn();
-
     }
-
-    search_weather_info(Selected_city_info);
-    
+    search_weather_info(Selected_city_info);   
+    }catch{
+        console.error();
+    }  
 }
+//this function is call every time the information must update when changing the city
+//there are the API calls and the function method that will fetch the data
 function search_weather_info(city_info){
 
     webAPI5 = 'https://api.openweathermap.org/data/2.5/forecast?lat='+city_info.lat+'&lon='+city_info.lng+'&appid='+appid+'&units=metric'
@@ -166,6 +181,8 @@ function search_weather_info(city_info){
     call_API5(webAPI5);
 }
 
+//this function resets the button list so then it would get fill with the btn_array
+//data it add the click event aswell
 function render_btn(){
     doc_btn_list.innerHTML = '';
 
@@ -185,7 +202,10 @@ function render_btn(){
     }
 
 }
-
+//retrieves the necessary data from the object passed down into the function 
+//it will get the data and format it into our wanted format
+//at the end it calls the function `render_today` which will show the info in
+//screen
 function todayCast(data){
     Selected_city_today = {
         date: dayjs.unix(data.dt).format('MM/D/YYYY'),
@@ -195,10 +215,12 @@ function todayCast(data){
         wind: data.wind.speed,
         humd: data.main.humidity
     };
-
+    
     render_today();
-
+    
 }
+//Renders todays data information into the upped daschboard using the global
+//variable used for the selected city data
 function render_today(){
     doc_today_city.textContent = Selected_city_info.name+', '+Selected_city_info.country+' ('+Selected_city_today.date+')';
     doc_today_icon.setAttribute('src',''+Selected_city_today.icon);
@@ -208,13 +230,15 @@ function render_today(){
     doc_today_humd.textContent = 'Humidity: '+Selected_city_today.humd+'%';
 
 }
-
+//retrieves the necessary data from the object passed down into the function 
+//the data outputs 6 object per day, so it retieve the wanted data from selected
+//object and formated into out wanted format and store on a 5 object array
+//at the end it calls the function `render_5day` which will show the info in
+//screen
 function Five_dayCast(data){
-
     Selected_city_5day = [];
     data.forEach(element => {
         if(dayjs.unix(element.dt).format('H') == 12){
-
             //console.log(element.dt);
             // console.log(element);
             var Selected_city_week = {
@@ -232,7 +256,8 @@ function Five_dayCast(data){
     //console.log(Selected_city_5day);
     render_5day();
 }
-
+//Renders the five day forecast data information into the information cards 
+//using the global variable used for the `Selected_city_5day` array
 function render_5day(){
     doc_5day_cast.innerHTML = '';
 
